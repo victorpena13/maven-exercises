8c837940bef6822aee1a0953b0f4e34449e4a95f
victor pena <victor.pena1392@Gmail.com>
2022-06-19 23:06:25 -0500

    introduced @Test annotation-tells JUnit that the public void method to which it is attached can be run as a test case. introduced assertions - assertEquals and assertNotEquals

--- src/test/java/DependencyManagement.java
+++ src/test/java/DependencyManagement.java
@@ -6,6 +6,23 @@ import java.util.Scanner;
 
 public class DependencyManagement {
 
+    @Test
+    public void testIfNameIsEqual() {
+        String expected = "test";
+        String actual = "Test";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userString = scanner.next();
